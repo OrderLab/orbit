bb13cb643d6d4ec12ce9bb34ac0e9b1f17e44abb
robertking <superobertking@icloud.com>
2022-04-15 22:12:49 -0400

    Update varnish script

--- apps/varnish/build.sh
+++ apps/varnish/build.sh
@@ -8,14 +8,16 @@ module load userlib/reuse
 git clone git@github.com:OrderLab/obiwan-varnish.git code
 cd code
 
-git checkout 4d36eae9
-./autogen.sh
-./configure --prefix=$SCRIPT_DIR/rel-orbit LDFLAGS='-lorbit' CFLAGS="-O2 -ggdb"
-make -j$(nproc)
-make install
-
 git checkout c5626ecea
 ./autogen.sh
 ./configure --prefix=$SCRIPT_DIR/rel-orig CFLAGS="-O2 -ggdb"
 make -j$(nproc)
 make install
+make clean -f
+
+git checkout 4d36eae9
+./autogen.sh
+./configure --prefix=$SCRIPT_DIR/rel-orbit LDFLAGS='-lorbit' CFLAGS="-O2 -ggdb"
+make -j$(nproc)
+make install
+make clean -f
